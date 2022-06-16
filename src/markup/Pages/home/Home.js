9bd38a6af1f9f8cde8a31ad997d1f1c141fa9116
Sebastian Murcia <smurcia162@gmail.com>
2022-06-16 10:20:52 -0500

    home banner

--- src/markup/Pages/home/Home.js
+++ src/markup/Pages/home/Home.js
@@ -1,13 +1,126 @@
 import IndexBanner from 'markup/Element/IndexBanner'
 import React from 'react'
+import banner from 'images/main-slider/banner_work.jpg'
+import { WrapperHome } from '.'
 
 const Home = () => {
   return (
-    <div className="page-wraper">
-      <div className="page-content">
-        <IndexBanner />
+    <WrapperHome className="home">
+      <div
+        className="home__image d-flex justify-content-center align-items-center flex-wrap"
+        style={{ backgroundImage: 'url(' + banner + ')' }}
+      >
+        <div className="home__form d-flex flex-column p-4">
+          <div className="d-flex flex-wrap flex-lg-nowrap home__form-row">
+            <div className="home__form-cont home__">
+              <input placeholder="Titulo del trabajo" className="w-100 home__form-input" name="title" />
+              <i className="fa fa-search home__form-icon" />
+            </div>
+            <div className="">
+              <button className="home__form-button">Buscar trabajo</button>
+            </div>
+          </div>
+          <div className="d-flex flex-wrap flex-lg-nowrap home__form-row ">
+            <div className="col-lg-5 home__form-cont ">
+              <select className="w-100 home__form-select ">
+                <option>Area funcional</option>
+                <option>Construction</option>
+                <option>Corodinator</option>
+                <option>Employer</option>
+                <option>Financial Career</option>
+                <option>Information Technology</option>
+                <option>Marketing</option>
+                <option>Quality check</option>
+                <option>Real Estate</option>
+                <option>Sales</option>
+                <option>Supporting</option>
+                <option>Teaching</option>
+              </select>
+            </div>
+            <div className="col-lg-3 home__form-cont">
+              <select className="w-100 home__form-select">
+                <option>Departamento</option>
+                <option>Construction</option>
+                <option>Corodinator</option>
+                <option>Employer</option>
+                <option>Financial Career</option>
+                <option>Information Technology</option>
+                <option>Marketing</option>
+                <option>Quality check</option>
+                <option>Real Estate</option>
+                <option>Sales</option>
+                <option>Supporting</option>
+                <option>Teaching</option>
+              </select>
+            </div>
+            <div className="col-lg-3 home__form-cont">
+              <select className="w-100 home__form-select">
+                <option>Ciudad</option>
+                <option>Construction</option>
+                <option>Corodinator</option>
+                <option>Employer</option>
+                <option>Financial Career</option>
+                <option>Information Technology</option>
+                <option>Marketing</option>
+                <option>Quality check</option>
+                <option>Real Estate</option>
+                <option>Sales</option>
+                <option>Supporting</option>
+                <option>Teaching</option>
+              </select>
+            </div>
+          </div>
+          <div className="border d-flex">
+            <div className="flex-1">
+              <select className="w-100 home__form-select ">
+                <option>Area funcional</option>
+                <option>Construction</option>
+                <option>Corodinator</option>
+                <option>Employer</option>
+                <option>Financial Career</option>
+                <option>Information Technology</option>
+                <option>Marketing</option>
+                <option>Quality check</option>
+                <option>Real Estate</option>
+                <option>Sales</option>
+                <option>Supporting</option>
+                <option>Teaching</option>
+              </select>
+            </div>
+            <div className="flex-1 d-flex gap-2">
+            <select className="w-100 home__form-select ">
+                <option>Area funcional</option>
+                <option>Construction</option>
+                <option>Corodinator</option>
+                <option>Employer</option>
+                <option>Financial Career</option>
+                <option>Information Technology</option>
+                <option>Marketing</option>
+                <option>Quality check</option>
+                <option>Real Estate</option>
+                <option>Sales</option>
+                <option>Supporting</option>
+                <option>Teaching</option>
+              </select>
+              <select className="w-100 home__form-select ">
+                <option>Area funcional</option>
+                <option>Construction</option>
+                <option>Corodinator</option>
+                <option>Employer</option>
+                <option>Financial Career</option>
+                <option>Information Technology</option>
+                <option>Marketing</option>
+                <option>Quality check</option>
+                <option>Real Estate</option>
+                <option>Sales</option>
+                <option>Supporting</option>
+                <option>Teaching</option>
+              </select>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </WrapperHome>
   )
 }
 export default Home
--- src/markup/Pages/home/index.js
+++ src/markup/Pages/home/index.js
@@ -1,1 +1,68 @@
+import styled from 'styled-components'
+
 export { default } from './Home'
+
+export const WrapperHome = styled.div`
+  .home {
+    &__image {
+      height: 600px;
+      width: 100%;
+      background-size: cover;
+      background-position: center center;
+    }
+    &__form {
+      box-sizing: border-box;
+      min-height: 200px;
+      background-color: white;
+      width: 80%;
+      padding:15px;
+
+      &-row {
+        display: flex;
+        width: 100%;
+        box-sizing: border-box;
+        gap:11px;
+      }
+
+      &-cont {
+        border-bottom: 3px solid var(--blue);
+        width: 90%;
+        box-sizing: border-box;
+        margin-left: 10px;
+        margin-right: 10px;
+
+      }
+
+      &-select {
+        height: 50px;
+        border: none;
+        margin-left: 10px;
+        outline:none;
+      }
+
+      &-button{
+        background-color:var(--blue);
+        border:none;
+        border-radius:5px;
+        padding:10px 15px;
+        width:100%;
+        min-width:200px;
+        font-size:1em;
+        color:white;
+        margin-top:10px;
+      }
+
+      &-input{
+        border:none;
+        height:50px;
+        outline:none;
+      }
+
+      &-icon{
+        position:absolute;
+        top:20px;
+        right:10px
+      }
+    }
+  }
+`
